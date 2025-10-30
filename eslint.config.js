1646fdcfe835e8cbbb5112a06931ceebeed20356
dobleduche <masebk1@gmail.com>
2025-10-30 11:54:56 -0400

    Configure ESLint for TypeScript and React
    Added TypeScript support and updated ESLint rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,26 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ["dist/*"],
-  }
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2023,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'react/react-in-jsx-scope': 'off', // React 18
+    },
+    ignores: ['dist/*', 'node_modules/*'],
+  },
 ]);
